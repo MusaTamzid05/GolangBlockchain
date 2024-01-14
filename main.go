db6b13609b623bf0eb77b1d315bd867ee7b4fe78
MusaTamzid05 <tamzid101@gmail.com>
2024-01-14 15:51:30 +0600

    Post handler for mining block added

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import  (
     "net/http"
 )
 
+type RequestData struct {
+    Data string `json:"data"`
+}
+
 var chain lib.BlockChain
 
 func init() {
@@ -18,10 +22,31 @@ func init() {
 func GetBlockChainHandler(c *gin.Context) {
     blocks := chain.Blocks
     c.IndentedJSON(http.StatusOK, blocks)
-
-
-
 }
+
+func MineBlockHandler(c *gin.Context) {
+    var requestData RequestData
+    err := c.BindJSON(&requestData)
+
+    if err != nil {
+        c.JSON(http.StatusBadRequest, gin.H {
+            "success" : false,
+            "message" : err.Error(),
+
+        })
+
+        return
+    }
+
+    chain.Add(requestData.Data)
+
+    c.JSON(http.StatusCreated, gin.H {
+        "success" : true,
+        "message" : "",
+
+    })
+}
+
 
 func main() {
     /*
@@ -45,6 +70,7 @@ func main() {
 
     router := gin.Default()
     router.GET("/blocks", GetBlockChainHandler)
+    router.POST("/mine", MineBlockHandler)
     router.Run()
 
 
